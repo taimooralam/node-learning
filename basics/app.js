ef3c57801bf5ebe83146d198807761c57dd33afd
Taimoor <taimoor.alam12@gmail.com>
2022-11-19 17:28:01 +0100

    send http response

--- basics/app.js
+++ basics/app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 
 const server = http.createServer((req, res ) => {
-    console.log(req);
+    console.log(req.url, req.method);
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>hello from my nodejs server</h1></body>');
+    res.write('</html>');
+    res.end();
+    process.exit();
 });
 
 server.listen(3000);
